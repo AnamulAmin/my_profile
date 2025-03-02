415caabf9da524b359fa2287c8aed8a43ebe2f0f
anamulamin <aadelbanat8991@gmail.com>
2025-03-02 23:48:30 +0600

    modifyed some areas

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../../components/Banner";
+import ContactSection from "../../components/ContactSection";
+import MainTag from "../../components/MainTag";
+function contact() {
+  return (
+    <MainTag>
+      <Banner bannerTitle="Contact Me" />
+      <ContactSection />
+    </MainTag>
+  );
+}
+
+export default contact;
