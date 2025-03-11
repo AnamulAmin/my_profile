94018c6d7c6cbccc208aa25958fade67a46d8602
anamulamin <aadelbanat8991@gmail.com>
2025-03-12 00:01:53 +0600

    included favicon image'

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,6 +31,11 @@ export default function RootLayout({ children }) {
   };
   return (
     <html lang="en">
+      <head>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        {/* You can also use a PNG or other image formats */}
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
